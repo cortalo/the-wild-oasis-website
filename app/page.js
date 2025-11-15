be89e75d3b12df6a3b9a05434e95c4f30b5e3098
Cortalo <Cortalo@outlook.com>
2025-11-15 10:01:54 +0100

    423. navigating between pages

--- app/page.js
+++ app/page.js
@@ -1,5 +1,13 @@
+import Link from "next/link";
+import Navigation from "./components/Navigation";
+
 function Page() {
-  return <h1>The Wild Oasis. Welcome to paradise.</h1>;
+  return (
+    <div>
+      <Navigation />
+      <h1>The Wild Oasis. Welcome to paradise.</h1>
+    </div>
+  );
 }
 
 export default Page;
